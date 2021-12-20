e498047a8268f8b545a9b800f31a8137e40e4ddc
Darian Lee <darianlmj@gmail.com>
2021-12-20 17:14:52 +1100

    Moved helper functions to helper file

--- asteroids.hpp
+++ asteroids.hpp
@@ -15,4 +15,14 @@
 #define NOT_INVALID 0
 #define INVALID 1
 
+void print_map(int map[MAP_SIZE][MAP_SIZE], int laserY);
+void copy_of_map(int map[MAP_SIZE][MAP_SIZE], int copy_map[MAP_SIZE][MAP_SIZE]);
+void place_blocks(int map[MAP_SIZE][MAP_SIZE]);
+void process_commands(int map[MAP_SIZE][MAP_SIZE]);
+void boom(int map[MAP_SIZE][MAP_SIZE], int laserY, int g, int stop_loop);
+void rotate_map(int map[MAP_SIZE][MAP_SIZE], int copy_map[MAP_SIZE][MAP_SIZE], int rotate);
+int check_if_lose(int map[MAP_SIZE][MAP_SIZE], int laserY);
+int check_if_win(int map[MAP_SIZE][MAP_SIZE], int laserY);
+void destroy_block(int map[MAP_SIZE][MAP_SIZE], int laserY);
+void shift_map(int map[MAP_SIZE][MAP_SIZE]);
 
